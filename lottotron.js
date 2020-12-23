b72a9dc13ec0c3216758f22dbf18e20b18d84406
Mikhail Snitko <mikhail.for.rs@gmail.com>
2020-12-24 00:56:31 +0300

    reafctor: Add comments into the lottotron.js.

--- lottotron.js
+++ lottotron.js
@@ -1,3 +1,12 @@
+
+/**
+ * Create an object that can return random non-repeated integers.
+ * It returns integers from 0 to the user-defined value.
+ *
+ * @param {number} maxNumber - The max number of the interval. Should be not less than 0. The float number will be rounded down to the nearest integer.
+ *
+ * @class Lottotron
+ */
 class Lottotron {
 
   constructor(maxNumber) {
@@ -9,26 +18,69 @@ class Lottotron {
       throw new Error('The input option "maxNumber" should be greater than 0.')
     }
 
+    /**
+     * The max number of the interval.
+     *
+     * @member {number} _maxNumber
+     * @memberof Lottotron
+     * @instance
+     * @private
+     */
     this._maxNumber = Math.floor(maxNumber)
 
+    /**
+     * The array of the numbers that were not returned from the *#getNumber()* method
+     * @member {number[]} _restNumbers -
+     * @memberof Lottotron
+     * @instance
+     * @private
+     */
     this._restNumbers = this._createNumbersArray(this._maxNumber)
 
   }
 
+  /**
+   * The max number of the interval
+   *
+   * @type {number}
+   *
+   * @readonly
+   * @memberof Lottotron
+   */
   get maxNumber() {
     return this._maxNumber;
   }
 
+  /**
+   * The array of the numbers that were not returned from method *#getNumber()*
+   *
+   * @type {number[]}
+   *
+   * @readonly
+   * @memberof Lottotron
+   */
   get restNumbers() {
     return this._cloneArray(this._restNumbers);
   }
 
+  /**
+   * Rallback this object to the inital state.
+   *
+   * @returns {undefined}
+   * @memberof Lottotron
+   */
   reload() {
     for (var i = 0; i <= this._maxNumber; i++) {
       this._restNumbers[i] = i
     }
   }
 
+  /**
+   * Return the next number until all numbers of the inteval are returned. Return "null" when all numbers have been returned.
+   *
+   * @returns {number|null}
+   * @memberof Lottotron
+   */
   getNumber() {
     if (this._restNumbers.length <= 0) {
       return null
@@ -38,6 +90,14 @@ class Lottotron {
     }
   }
 
+  /**
+   * Return a clone of the array
+   *
+   * @param {array} array
+   * @returns {array}
+   * @memberof Lottotron
+   * @private
+   */
   _cloneArray(array) {
     var res = []
     array.forEach(function(value, i, array) {
@@ -46,12 +106,31 @@ class Lottotron {
     return res
   }
 
+  /**
+   * Return a random number from min to max
+   *
+   * @param {number} min
+   * @param {number} max
+   *
+   * @returns {number}
+   * @memberof Lottotron
+   * @private
+   */
   _randomInteger(min, max) {
     var rand = min + Math.random() * (max + 1 - min)
     rand = Math.floor(rand)
     return rand
   }
 
+  /**
+   * Return an integer array filled with numbers from 0 to "maxNumber".
+   *
+   * @param {number} maxNumber
+   *
+   * @returns {array}
+   * @memberof Lottotron
+   * @private
+   */
   _createNumbersArray(maxNumber) {
     var res = []
       for (var i = 0; i <= maxNumber; i++) {
