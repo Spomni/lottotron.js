7000b8664052c69a7863d8abd47981ef641e7b7b
Spomni <mikhail.snitko@gmail.com>
2020-12-31 00:58:15 +0200

    Refactor tests to ES2015 (#11)
    * refactor: Refactor the lottotron.spec.js file.
    * refactor: Refactor the LottotronError.spec.js file.

--- test/lottotron.spec.js
+++ test/lottotron.spec.js
@@ -1,325 +1,275 @@
 const assert = require('chai').assert
-const Lottotron = require('../lottotron.js')
-const lottotronError = require('../lib/LottotronError');
 
-const assertThrowsLottotronError = (cb) => {
-  assert.throws(cb);
-  try {
-    cb();
-  } catch (error) {
-    assert.instanceOf(error, lottotronError);
-  }
+const Lottotron = require('../lottotron.js')
+const LottotronError = require('../lib/LottotronError');
+
+const arrayOfTypes = [
+  'string',
+  null,
+  undefined,
+  [],
+  {},
+  () => {},
+
+  /* number */
+  1,
+  -1,
+  1.3,
+  -1.4,
+  +Infinity,
+  -Infinity,
+  NaN,
+]
+
+const ERROR_MSG_ ={
+  MAX_NUMBER_IS_NOT_NUMBER: 'The input option "maxNumber" should be a number.',
+  MAX_NUMBER_LOWER_ZERO: 'The input option "maxNumber" should be greater than 0.',
+  MAX_NUMBER_IS_NON_FINITE: 'The input option "maxNumber" should be a finite number.'
 }
 
-describe('lottotron.js', function() {
-  /** @namespace itWrappers */
-    const itWr = {
+const isNumber = (value) => typeof(value) === 'number';
 
-      /** @function forEachType Вызывает функцию callback для каждого из стандартных типов данных.
-        * @describe Вызывает функцию callback для каждого из стандартных типов данных. В параметр type передает экземпляр соответствующего типа данеых.
-        * @param {Function} callback( type )
-        * @returns {undefined}
-        */
-        forEachType : function(callback) {
-          var typeArr = [
-            1,
-            'string',
-            null,
-            undefined,
-            [],
-            {}
-          ]
+const getNaturalNumbersTo = (maxNumber) => {
+  let array = [];
+  for (let i=0; i <= maxNumber; i++) array.push(i);
+  return array;
+}
 
-          var res = function() {
-            typeArr.forEach(function(type, i, typeArr) {
-              callback(type)
-            })
-          }
+describe('class Lottotron', () => {
 
-          return res
-        }
-      //
-    } // itWrapper
+  describe('constructor(maxNumber)', () => {
 
-  /** @function isNumber
-    * @param {mixed} value
-    * @returns {boolean}
-    */
-    function isNumber(param) {
-      return (typeof(param) === 'number')
-    }
+    it('Should throw an error if the maxNumber option is not a number.',
+      () => arrayOfTypes.forEach((typeValue) => {
+        if (isNumber(typeValue)) return;
 
-  /** @function isNull
-    * @param {mixed} value
-    * @returns {boolean}
-    */
-    function isNull(value) {
-      return value === null
-    }
-
-  /** @function doesArrayInclude Check the inclusion of the value in the array.
-    * @desc Check the inclusion of the value in the array. Return "true" if the value is encluded else return "false".
-    *
-    * @param {array} array
-    * @param {mixed} value
-    *
-    * @returns {boolean}
-    */
-    function doesArrayInclude(array, value) {
-      var res = false
-      array.forEach(function(arrayValue, i, array) {
-        if (arrayValue === value) {
-          res = true
-        }
+        assert.throws(
+          () => new Lottotron(typeValue),
+          LottotronError,
+          ERROR_MSG_.MAX_NUMBER_IS_NOT_NUMBER
+        );
       })
-      return res
-    }
+    );
 
-  /** @function isEqualArrays
-    * @param {array} array1
-    * @param {array} array2
-    * @returns {boolean}
-    */
-    function isEqualArrays(array1, array2) {
-      if (array1.length !== array2.length) {
-        return false
-      }
-      for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          return false
-        }
-      }
-      return true
-    }
+    it(`Should throw an error if the maxNumber option is a non-finite number.`,
+      () => arrayOfTypes.forEach((typeValue) => {
+        if (!isNumber(typeValue)) return;
+        if (Number.isFinite(typeValue)) return;
 
-  describe('new Lottotron( maxNumber )', function() {
-    it('Should throw an "Error" object if the input param "maxNumber" is not number.', itWr.forEachType(function(type) {
-      if (!isNumber(type)) {
-        assert.throws(function() {
-          var lotto = new Lottotron(type)
-          lotto.reset()
-        }, Error)
-      }
-    }))
+        assert.throws(
+          () => new Lottotron(typeValue),
+          LottotronError,
+          ERROR_MSG_.MAX_NUMBER_IS_NON_FINITE
+        )
+      })
+    );
 
-
-    it('Should throw an "Error" object if the input param "maxNumber" is not finite value',
+    it('Should throw an error if the maxNumber option less than 0.',
       () => {
-        [NaN, +Infinity, -Infinity].forEach((nonFinite) => {
-          assertThrowsLottotronError(() => new Lottotron(nonFinite));
-        })
+        assert.throws(
+          () => new Lottotron(-5),
+          LottotronError,
+          ERROR_MSG_.MAX_NUMBER_LOWER_ZERO
+        )
+      }
+    )
+
+    it('Should return a Lottotron instance if the maxNumber option has a correct value.',
+      () => assert.instanceOf(new Lottotron(3), Lottotron)
+    )
+  })
+
+  describe('#maxNumber', () => {
+
+    it(`Should be a positive integer`,
+      () => {
+        const { maxNumber } = new Lottotron(3.6);
+        assert(Number.isInteger(maxNumber))
+      }
+    )
+
+    it(`Should be equal to the maxNumber constructor option if one passed with integer value.`,
+      () => {
+        const value = 5;
+        const { maxNumber } = new Lottotron(value);
+        assert.strictEqual(maxNumber, value);
+      }
+    )
+
+    it(`Should be equal the rounded down maxNumber constructor option if one passed with a float value.`,
+      () => {
+        const value = 93.4;
+        const flooredValue = Math.floor(value);
+        const { maxNumber } = new Lottotron(value);
+        assert.strictEqual(maxNumber, flooredValue);
+      }
+    )
+
+    it('Should be a read-only property.',
+      () => {
+        const initalValue = 3;
+        const lotto = new Lottotron(initalValue);
+        lotto.maxNumber = 7;
+        assert.equal(lotto.maxNumber, initalValue);
+      }
+    )
+  })
+
+  describe('#getNumber()', () => {
+
+    it(`Should return all numbers of the interval in (maxNumber + 1) method calls.`,
+      () => {
+        const lotto = new Lottotron(8);
+        const { maxNumber } = lotto;
+
+        assert(
+          Array(maxNumber + 1).fill(null)
+            .map(() => lotto.getNumber())
+            .every((number, index, numberList) => numberList.includes(index))
+        )
       }
     );
 
-    it('Should throw an "Error" object if the input param "maxNumber" is less than 0.', function() {
-      assertThrowsLottotronError(function() {
-        var lotto = new Lottotron(-1)
-        lotto.reset()
-      }, Error)
-    })
+    it(`Should return null if method is called (maxNumber + 2) or more times. `,
+      () => {
+        const lotto = new Lottotron(6);
+        const { maxNumber } = lotto;
+        let counter = 0;
 
-    it('Should return an object instance of "Lottotron" if the input param "maxNumber" is number and it is not less than 0.', function() {
-      var lotto = new Lottotron(8)
-      assert.instanceOf(lotto, Lottotron)
-    })
+        while (counter < maxNumber + 5) {
+          if (counter <= maxNumber + 1) {
+            lotto.getNumber()
+          } else {
+            assert.isNull(lotto.getNumber());
+          }
+          counter++;
+        }
+      }
+    );
 
-    it('Should round to down the param "maxNumber" if it is a float number.', function() {
-      var lotto = new Lottotron(4.7)
-      assert.strictEqual(lotto.maxNumber, 4)
-    })
+    it(`Should not return equal number sequenses from different Lottotron instance.`,
+      () => {
+        const maxNumber = 8;
+        const lotto1 = new Lottotron(maxNumber);
+        const lotto2 = new Lottotron(maxNumber);
+
+        const sequence1 = Array(maxNumber + 1).fill(null)
+          .map(() => lotto1.getNumber())
+
+        const sequence2 = Array(maxNumber + 1).fill(null)
+          .map(() => lotto2.getNumber())
+
+        assert.isFalse(sequence1.every((number, index) => {
+          return sequence2[index] === number;
+        }))
+      }
+    )
   })
 
-  describe('Lottotron', function() {
-    describe('#maxNumber', function() {
-      it('Should be number', function() {
-        var lotto = new Lottotron(8.3)
-        assert.isNumber(lotto.maxNumber)
-      })
+  describe('#restNumbers', () => {
 
-      it('Should be not less than 0', function() {
-        var lotto = new Lottotron(5.7)
-        assert(!(lotto.maxNumber < 0))
-      })
+    it(`Should be an array.`,
+      () => {
+        const { restNumbers } = new Lottotron(32);
+        assert.isArray(restNumbers);
+      }
+    );
 
-      it('Should be integer', function() {
-        var lotto = new Lottotron(3.2)
-        assert((lotto.maxNumber % 1 === 0))
-      })
+    it('Should contain all numbers of the interval that were not returned from the method "getNumber".',
+      () => {
+        const maxNumber = 5;
+        const lotto = new Lottotron(maxNumber);
 
-      it('Should be read-only.', function() {
-        var lotto = new Lottotron(3)
-        lotto.maxNumber = 7
-        assert.equal(lotto.maxNumber, 3)
-      })
-    })
+        let remainder = Array(maxNumber + 1).fill(null)
+          .map((value, index) => index);
 
-    describe('#getNumber()', function() {
-      it('The calls with numbers from 0 till "maxNumber" should return all numbers of the interval.', function() {
-        var maxNumber = 4
-        var lotto = new Lottotron(maxNumber)
-        var returnedNumbers = []
+        for (let i=0; i <= maxNumber; i++) {
+          const { restNumbers } = lotto;
 
-        for (let i = 0; i <= maxNumber; i++) {
-          returnedNumbers.push(lotto.getNumber())
+          assert.deepEqual(restNumbers, remainder);
+
+          const number = lotto.getNumber();
+          const index = remainder.findIndex((value) => value === number);
+          remainder.splice(index, 1);
         }
+      }
+    );
 
-        for (let i = 0; i <= maxNumber; i++) {
-          assert.include(returnedNumbers, i)
-        }
-      })
+    it(`Should return an empty array if all numbers from the interval was returned`,
+      () => {
+        const maxNumber = 13;
+        const lotto = new Lottotron(13)
+        let counter = 0;
 
-      it('The next calls should return "null"', function() {
-        var maxNumber = 4
-        var lotto = new Lottotron(maxNumber)
-
-        for (let i = 0; i <= maxNumber; i++) {
-          lotto.getNumber()
-        }
-
-        assert.isNull(lotto.getNumber())
-        assert.isNull(lotto.getNumber())
-      })
-
-      it('The numbers sequences, returned from the different "Lottotron" instances, should not be equal.', function() {
-        var maxNumber = 19
-        var lotto1 = new Lottotron(maxNumber)
-        var lotto2 = new Lottotron(maxNumber)
-        var array1 = []
-        var array2 = []
-
-        for (let i = 0; i <= maxNumber; i++) {
-          array1.push(lotto1.getNumber())
-          array2.push(lotto2.getNumber())
-        }
-
-        assert(!isEqualArrays(array1, array2))
-      })
-    })
-
-    describe('#restNumbers', function() {
-      it('Should be an array', function() {
-        var lotto = new Lottotron(4)
-        assert.isArray(lotto.restNumbers)
-      })
-
-      it('Should contain all numbers of the interval that were not returned from the method "getNumber".', function() {
-        var maxNumber = 6
-        var lotto = new Lottotron(maxNumber)
-
-        var dontReturned = []
-        for (let i = 0; i <= maxNumber; i++) {
-          dontReturned.push(i)
-        }
-
-        for (let i = 0; i <= maxNumber; i++) {
-          var number = lotto.getNumber()
-
-          for (var key in dontReturned) {
-            if (dontReturned[key] === number) {
-              dontReturned.splice(key, 1)
-            }
+        while (counter < maxNumber + 7) {
+          lotto.getNumber();
+          if (counter > maxNumber + 1) {
+            assert.isEmpty(lotto.restNumbers);
           }
-
-          dontReturned.forEach(function(number, i, dontReturned) {
-            assert.include(lotto.restNumbers, number)
-          })
+          counter++;
         }
-      })
+      }
+    );
 
-      it('Should not contain numbers that was returned from the method "getNumber".', function() {
-        var maxNumber = 9
-        var lotto = new Lottotron(maxNumber)
-        var returnedNumbers = []
+    it(`Should return new array every time method calls.`,
+      () => {
+        const lotto = new Lottotron(3);
 
-        for (let i = 0; i <= maxNumber; i++) {
-          returnedNumbers.push(lotto.getNumber())
-          var restNumbers = lotto.restNumbers
+        const oldValue = lotto.restNumbers;
+        lotto.restNumbers = Symbol('newValue');
+        assert(lotto.restNumbers !== oldValue);
+      }
+    )
 
-          returnedNumbers.forEach(function(number, i, returnedNumbers) {
-            assert.notInclude(restNumbers, number)
-          })
-        }
-      })
+    it('Should be a read-only property.',
+      () => {
+        const lotto = new Lottotron(3);
 
-      it('Should return an empty array if all numbers from the interval was returned from the method "getNumber".', function() {
-        var lotto = new Lottotron(7)
+        const oldValue = lotto.restNumbers;
+        lotto.restNumbers = Symbol('newValue');
+        assert.deepEqual(lotto.restNumbers, oldValue);
+      }
+    )
 
-        while (!isNull(lotto.getNumber())) {
-          'smile please'
-        };
+    it(`Should return the same result if the array returned in the previous call is changed.`,
+      () => {
+        const lotto = new Lottotron(13);
+        const { restNumbers } = lotto;
+        const restNumbersCopy = restNumbers.map((item) => item);
+        restNumbers[6] = Symbol('someValue');
+        assert.deepEqual(lotto.restNumbers, restNumbersCopy);
+      }
+    );
+  })
 
-        assert.isArray(lotto.restNumbers)
-        assert.strictEqual(lotto.restNumbers.length, 0)
-      })
+  describe('#reload()', () => {
 
-      it('Should be read-only.', function() {
-        var lotto = new Lottotron(4)
+    it(`The #restNumbers array should contain all numbers after that the #reload()was called.`,
+      () => {
+        const maxNumber = 9;
+        const lotto = new Lottotron(maxNumber);
+        const expectedRestNumbers = getNaturalNumbersTo(maxNumber);
 
-        lotto.restNumbers = [1, 3]
-
-        assert(isEqualArrays([0, 1, 2, 3, 4], lotto.restNumbers))
-      })
-
-      it('Should not change when the returb value has been changed.', function() {
-        var lotto = new Lottotron(3)
-
-        var value = lotto.restNumbers
-        value.push(98)
-
-        assert(isEqualArrays([0, 1, 2, 3], lotto.restNumbers))
-      })
-    })
-
-    describe('#reload()', function() {
-      it('The property "restNumbers" should contain all numbers of the interval.', function() {
-        var maxNumber = 11
-        var lotto = new Lottotron(maxNumber)
-
-        var initalArray = []
-        for (let i = 0; i <= maxNumber; i++) {
-          initalArray.push(i)
-        }
-
-        for (let i = 0; i < 5; i++) {
-          lotto.getNumber()
-        }
-
+        while (lotto.restNumbers.length > 2) lotto.getNumber();
         lotto.reload()
+        assert.deepEqual(lotto.restNumbers, expectedRestNumbers)
+      }
+    );
 
-        initalArray.forEach(function(number, i, initalArray) {
-          assert.include(lotto.restNumbers, number)
-        })
-      })
+    it(`The #geNumber() method should return all numbers of the interval in (maxNumber + 1) method calls after that the #reload() method was called.`,
+      () => {
+        const maxNumber = 17;
+        const lotto = new Lottotron(maxNumber);
 
-      it('The "#getNumber()" calls with numbers from 0 till "maxNumber" should return all numbers of the interval..', function() {
-        var maxNumber = 12
-        var lotto = new Lottotron(maxNumber)
+        while (lotto.restNumbers.length > 0) lotto.getNumber();
+        lotto.reload();
 
-        var numbersArray = []
-        for (let i = 0; i <= maxNumber; i++) {
-          numbersArray.push(i)
-        }
-
-        for (let i = 0; i < 5; i++) {
-          lotto.getNumber()
-        }
-
-        lotto.reload()
-
-        for (let i = 0; i <= maxNumber; i++) {
-          var number = lotto.getNumber()
-
-          if (doesArrayInclude(numbersArray, number)) {
-            numbersArray.forEach(function(value, index, numbersArray) {
-              if (value === number) {
-                numbersArray.splice(index, 1)
-              }
-            })
-          }
-        }
-
-        assert.strictEqual(numbersArray.length, 0)
-      })
-    })
+        assert(
+          Array(maxNumber + 1).fill(null)
+            .map(() => lotto.getNumber())
+            .every((number, index, numberList) => numberList.includes(index))
+        )
+      }
+    );
   })
 })
