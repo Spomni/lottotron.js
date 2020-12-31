5dd313f986d2ecad25308efd9e86912d30a620fb
Spomni <mikhail.snitko@gmail.com>
2020-12-31 04:25:21 +0200

    style: Lint code. (#13)

--- test/LoottotronError.spec.js
+++ test/LoottotronError.spec.js
@@ -1,28 +1,25 @@
-const { assert } = require('chai');
+const { assert } = require('chai')
 
-const LottotronError = require('../lib/LottotronError');
+const LottotronError = require('../lib/LottotronError')
 
 describe(`class LottotronError`, () => {
-
   describe(`constructor(message):`, () => {
-
     it(`Should be an instance of LottotronError.`, () => {
-      assert.instanceOf(new LottotronError(), LottotronError);
+      assert.instanceOf(new LottotronError(), LottotronError)
     })
 
     it(`Should be an instance of Error.`, () => {
-      assert.instanceOf(new LottotronError(), Error);
+      assert.instanceOf(new LottotronError(), Error)
     })
   })
 
   describe('#message', () => {
     it(`Should be equal to the option passed in constructor.`,
       () => {
-        const string = `message`;
-        const { message } = new LottotronError(string);
-        assert.strictEqual(message, string);
+        const string = `message`
+        const { message } = new LottotronError(string)
+        assert.strictEqual(message, string)
       }
-    );
+    )
   })
-
 })
